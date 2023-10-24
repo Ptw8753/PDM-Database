591a8212095f7b5de48ed276a1b4018022c52319
jwh1000 <jwh1000@rit.edu>
2023-10-24 09:09:16 -0400

    added .json file usage for getting username and password

--- main.py
+++ main.py
@@ -1,40 +1,29 @@
 import os
 import psycopg2
-import json
 
 from sshtunnel import SSHTunnelForwarder
 
-def get_login():
-    with open("login.json") as f:
-        credentials = json.loads(f.read())
+with SSHTunnelForwarder(
+        ('starbug.cs.rit.edu', 22),  # port 22 as standard SSH port
+        ssh_username=os.environ.get("Username"),
+        ssh_password=os.environ.get("Password"),
+        remote_bind_address=('127.0.0.1', 5432)) as server:  # mirroring to local port 5432
 
-        print(credentials)
-        return (credentials['username'], credentials['password'])
+    server.start()
 
-def main():
-    username, password = get_login()
-    with SSHTunnelForwarder(
-            ('starbug.cs.rit.edu', 22),  # port 22 as standard SSH port
-            ssh_username=username,
-            ssh_password=password,
-            remote_bind_address=('127.0.0.1', 5432)) as server:  # mirroring to local port 5432
+    params = {  # database params
+        'database': 'p320_18',
+        'user': os.environ.get("Username"),
+        'password': os.environ.get("Password"),
+        'host': '127.0.0.1',
+        'port': server.local_bind_port
+    }
+    conn = psycopg2.connect(**params)
+    cur = conn.cursor()  # if this works, you are connected
+    print("DB connected")
+    cur.execute("Select * from \"Genre\"")
+    records = cur.fetchall()
+    for record in records:
+        print(record)
 
-        server.start()
 
-        params = {  # database params
-            'database': 'p320_18',
-            'user': username,
-            'password': password,
-            'host': '127.0.0.1',
-            'port': server.local_bind_port
-        }
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()  # if this works, you are connected
-        print("DB connected")
-        cur.execute("Select * from \"genre\"")
-        records = cur.fetchall()
-        for record in records:
-            print(record)
-
-if __name__ == '__main__':
-    main()
