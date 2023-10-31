8070f6b2ecfc5969e602ddfc3ffe9892c0b5d4b7
ajc5660 <ajc5660@rit.edu>
2023-10-31 17:47:47 -0400

    Moved random function to random_function.py

--- add_data.py
+++ add_data.py
@@ -1,9 +1,7 @@
 import json
-import datetime
-import random
+from random_functions import *
 from database import Database
-start_datetime = datetime.datetime(2018, 1, 1, 0, 0, 0)     # January 1, 2018 at 12:00:00 AM
-end_datetime = datetime.datetime(2022, 12, 31, 23, 59, 59)  # December 31, 2022 at 11:59:59 PM
+
 possible_genre_combinations = [ 
     ["Rap"], ["Reggae"], ["Country"], 
     ["Pop"], ["RnB"], ["Classical"], 
@@ -12,79 +10,6 @@ possible_genre_combinations = [
     ["Electronic", "Pop"], ["Electronic", "Funk"], 
     ["Pop", "Rock"] 
 ]
-
-
-
-# Generate 2^-i frequency distribution for i in [1:size]
-# n(i) > 2 * n(i + 1) for all 1 <= i <= size
-# n(i) != 0 for all 1 <= i <= size
-# sum({n(i) for all 1 <= i <= size}) = 1
-# limit size -> inf for get_inv2_frequencies(size) = [0.5, 0.25, 0.125, ...]
-def get_inv2_frequencies(size):
-    freqs = []
-    norm = (2 ** size - 1) / 2 ** size
-    for i in range(size):
-        freq = 2 ** -(i + 1)
-        freqs.append(freq / norm)
-    return freqs
-
-# Return an index given a list of frequencies for each index
-# Sum of freqs should be 1
-# freq(i) is the probability of i
-def random_index_freq(freqs):
-    total = 0
-    rval = random.random()
-    for i in range(len(freqs)):
-        total += freqs[i]
-        if rval < total:
-            return i
-    return len(freqs) - 1
-
-
-# Return random disjoint subsets of a given list with size specified upon call
-# List will be modified
-def random_subset_without_replacement(ls, min_size, max_size, size_freqs = None):
-    if size_freqs is not None: assert max_size - min_size == len(size_freqs) + 1
-    random.shuffle(ls)
-    i = 0
-    while i < len(ls):
-        size = random.randint(min_size, max_size) if size_freqs is None else random_index_freq(size_freqs) + min_size
-        yield ls[i : i + size]
-        i += size
-
-# Return random subsets of a given list
-def random_subset_with_replacement(ls, min_size, max_size, size_freqs = None):
-    if size_freqs is not None: assert max_size - min_size == len(size_freqs) + 1
-    while True:
-        size = random.randint(min_size, max_size) if size_freqs is None else random_index_freq(size_freqs) + min_size
-        yield random.choices(ls, k=size)
-
-
-max_n_genres = max([len(i) for i in possible_genre_combinations])
-genre_combinations_len_map = {i : [t for t in possible_genre_combinations if len(t) == i] for i in range(1, max_n_genres + 1)}
-
-# Random datetime from uniform distribution
-def random_datetime(start=start_datetime, end=end_datetime):
-    delta = end - start
-    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
-    random_second = random.randrange(int_delta)
-    return start + datetime.timedelta(seconds=random_second)
-
-# Random date from uniform distribution
-def random_date(start=start_datetime, end=end_datetime):
-    return random_datetime(start=start_datetime, end=end_datetime).date()
-
-# Convert a python datetime to an sql datetime
-def python_datetime_to_sql(python_dt : datetime.datetime):
-    f = '%Y-%m-%d %H:%M:%S'
-    return python_dt.strftime(f)
-
-# Convert a python date to an sql date
-def python_date_to_sql(python_d : datetime.date):
-    f = '%Y-%m-%d'
-    y, m, d = python_d.year, python_d.month, python_d.day
-    python_dt = datetime.datetime(y, m, d, 0, 0)
-    return python_dt.strftime(f)
 
 if __name__ == "__main__":
     
