565dd141a5f603b8b067570e5b88c3771965f03c
jwh1000 <jwh1000@rit.edu>
2023-10-31 09:51:59 -0400

    added some console stuff to cli

--- cli.py
+++ cli.py
@@ -3,6 +3,7 @@ from db_interface import Interface
 # if this breaks for you, do "python -m pip install rich"
 from rich import print
 from rich.console import Console
+from rich.markdown import Markdown
 
 class Cli:
     def __init__(self, interface: Interface):
@@ -14,7 +15,16 @@ class Cli:
 
 
     def startup(self):
-        print()
+        MARKDOWN = """
+        # Principles of Data Management
+        ## Music Domain
+
+        Welcome! Please selection option from the below list:
+        * option a
+        * option b
+        * option c
+        """
+        self.console.print(Markdown(MARKDOWN))
 
 
     def input_loop(self):
@@ -23,5 +33,7 @@ class Cli:
         while(input_str.strip() != "quit" and input_str.strip() != "q"):
             
             
-            input_str = input()
+
+
+            input_str = self.console.input()
         
