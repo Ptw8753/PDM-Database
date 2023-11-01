7b5121408f020aa84c6ba0df2e47b967dcf7364e
jwh1000 <jwh1000@rit.edu>
2023-11-01 16:13:13 -0400

    added colors, search screen, and moved login to its own function

--- cli.py
+++ cli.py
@@ -11,70 +11,115 @@ class Cli:
         self.interface = interface
         self.console = Console()
         self.screen = "main"
+        self.loginId = None
 
-        self.startup()
+        self.render_heading()
         self.input_loop()
 
 
     def print_options(self):
         if self.screen == "main":
-            column1 ="""
+            column1 ="""[bright_green]
 * login \[username] \[password]  
 * signup  
 * collections  
-* search \[songname]  
+* search
 """
-            column2 ="""
+            column2 ="""[bright_green]
 * listen \[songname] or \[albumname]  
 * follow \[useremail]  
 * unfollow \[useremail]  
 * help \[command]  
 """
         elif self.screen == "collections":
-            column1 ="""
+            column1 ="""[bright_red]
 * create \[name]  
 * +album \[collectionname]  
 * +song \[cllectionname]  
 * editname \[name] \[newname]  
 """
 
-            column2 ="""
+            column2 ="""[bright_red]
 * delete \[name]  
 * -album \[collectionname]  
 * -song \[collectionname]  
 * listen \[collectionname]  
 """
+
+        elif self.screen == "search":
+            column1 ="""[bright_blue]
+* songs \[songname]
+* albums \[albumname]
+* artists \[artistname]
+* genres \[genre]
+"""
+            column2 =""""""
+
         self.console.print(Panel(Columns([column1, column2]), title="Command List"))
 
     
     def render_heading(self):
-        header = """# Principles of Data Management: Music Domain"""
-        self.console.print(Markdown(header))
+        header = """# Principles of Data Management: Music Domain
+Enter "quit" or "q" to """
 
+        if self.screen in ["collections", "search"]:
+            end = """return."""
+        else:
+            end = """exit."""
 
-    def startup(self):
-        self.render_heading()
+        if self.screen == "search":
+            self.console.print("Search in:")
+
+        self.console.print(Markdown(header + end))
         self.console.print()
+
+
+    def login(self, command):
+        if len(command) != 3:
+            self.console.print("Invalid arguments, usage: login \[username] \[password]") 
+            self.console.input("Press enter to continue...")
+        else:
+            self.interface.loginUser(command[1], command[2])
+            self.console.input("Press enter to continue...")
+
+
+    def signup(self, command):
+        # TODO: make new account here
+        pass
 
 
     def input_loop(self):
         input_str = "null"
 
-        while(input_str.split()[0] not in ["quit", "q"]):
+        while(True):
             self.print_options()
             input_str = self.console.input("> ")
             
             command = input_str.split()
-            if (command[0] == "login"):
-                if len(command) != 3:
-                    self.console.print("Invalid arguments, usage: login \[username] \[password]") 
-                    self.console.input("Press enter to continue...")
-                else:
-                    self.interface.loginUser(command[1], command[2])
-                    self.console.input("Press enter to continue...")
+            if (self.screen == "main"):
+                if (command[0] == "login"):
+                    self.login()
+
+                elif (command[0] == "signup"):
+                    self.signup()
+                
+                elif (command[0] == "collections"):
+                    self.screen = "collections"
+
+                elif (command[0] == "search"):
+                    self.screen = "search"
             
-            if (command[0] == "collections"):
-                self.screen = "collections"
+            elif (self.screen == "collections"):
+                pass
+
+            elif (self.screen == "search"):
+                pass
+
+            if (command[0]) in ["quit", "q"]:
+                if self.screen in ["collections", "search"]:
+                    self.screen = "main"
+                else:
+                    break
 
             self.console.clear()
             self.render_heading()
