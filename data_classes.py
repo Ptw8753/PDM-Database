e7db1bc05736081db0fe212d5f4dc6bb8502f180
Patrick Wynne <113530378+Ptw8753@users.noreply.github.com>
2023-11-02 12:47:08 -0400

    removed data classes that we don't need

--- data_classes.py
+++ data_classes.py
@@ -1,12 +1,6 @@
 from dataclasses import dataclass
 from datetime import *
 
-
-@dataclass
-class Genre:
-    name: str
-
-
 @dataclass
 class Song:
     title: str
@@ -15,34 +9,3 @@ class Song:
     genres: list[str]
     length: int
     listenCount: int
-
-
-@dataclass
-class Album:
-    name: str
-    songs: list[Song]
-    genres: list[Genre]
-    releaseDate: date
-
-
-@dataclass
-class Artist:
-    name: str
-
-
-@dataclass
-class Playlist:
-    name: str
-    songs: list[Song]
-    creationDate: date
-
-
-@dataclass
-class User:
-    username: str
-    password: str
-    firstName: str
-    lastName: str
-    email: str
-    creationDate: date
-    lastAccess: datetime
